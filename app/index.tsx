cabc9a1f770e4c5c949103e44c437d63baf1e02b
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-22 20:27:55 +0530

    add Tailwind CSS and NativeWind configuration

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to my Movie app!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-blue-600 font-bold">Welcome to my Movie app!</Text>
     </View>
   );
 }
