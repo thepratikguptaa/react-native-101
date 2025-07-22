9df474534a8638d829f01faccf7ff5671f98994f
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-22 21:03:46 +0530

    refactor: update welcome message and enhance Tailwind CSS theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-600 font-bold">Welcome to my Movie app!</Text>
+      <Text className="text-4xl text-dark-200 font-bold">Welcome to my app!</Text>
     </View>
   );
 }
