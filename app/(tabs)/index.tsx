50bc873d4406da8afabb37a4d43e70a7cf7bed25
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-23 17:58:19 +0530

    feat: implement tab navigation and create screens for Home, Search, Saved, and Profile

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-4xl text-dark-200 font-bold">
+        Welcome to my app!
+      </Text>
+    </View>
+  );
+}
