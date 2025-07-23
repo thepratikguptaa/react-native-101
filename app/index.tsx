d7fc6060be1226d5aa1dfbf774108bcc23f4c191
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-23 15:57:38 +0530

    feat: add onboarding and movie details screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,19 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-4xl text-dark-200 font-bold">Welcome to my app!</Text>
+      <Text className="text-4xl text-dark-200 font-bold">
+        Welcome to my app!
+      </Text>
+      <Link
+        className="text-2xl text-dark-200 m-4 px-4 py-2 border rounded-lg"
+        href="/onboarding"
+      >
+        Onboarding
+      </Link>
+      <Link className="text-2xl text-dark-200 m-4 px-4 py-2 border rounded-lg" href="/movie/avengers">Avengers Movie</Link>
     </View>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const onboarding = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-4xl text-dark-200 font-bold">This is onboarding screen</Text>
+        </View>
+    );
+};
+
+export default onboarding;
+
+const styles = StyleSheet.create({});
