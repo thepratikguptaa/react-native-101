20c82fffa40d5cc88c55d40a200f01efec5636db
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-27 16:45:05 +0530

    feat: implement search count tracking in Appwrite database

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -5,6 +5,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
@@ -21,9 +22,13 @@ const Search = () => {
     } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
     useEffect(() => {
+
         const timeoutId = setTimeout(async () => {
             if(searchQuery.trim()) {
                 await loadMovies();
+                if(movies?.length > 0 && movies?.[0]) {
+                    await updateSearchCount(searchQuery, movies[0]);
+                }
             } else {
                 reset()
             }
