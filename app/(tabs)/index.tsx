20750695f981e0eede9a61e8aae7b2c06ba06e33
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-27 17:38:36 +0530

    feat: add trending movies feature and update search count tracking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,29 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -26,14 +41,14 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        {moviesLoading ? (
-          <ActivityIndicator 
+        {moviesLoading || trendingLoading ? (
+          <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
@@ -41,33 +56,49 @@ export default function Index() {
               placeholder="Search for a movie"
             />
 
-            <>
-              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold">
+                  Trending Movies
+                </Text>
+              </View>
+            )}
 
-              <FlatList 
-                data={movies}
-                renderItem={({item}) => (
-                  <MovieCard 
-                    {...item}
-                  />
+            <>
+              
+              <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                ItemSeparatorComponent={() => <View className="w-4"/>}
+                className="mb-4 mt-3"
+                data={trendingMovies}
+                renderItem={({ item, index }) => (
+                  <TrendingCard movie={item} index={index}/>
                 )}
+                keyExtractor={(item) => item.movie_id.toString()}
+              />
+
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
                 keyExtractor={(item) => item.id.toString()}
                 numColumns={3}
                 columnWrapperStyle={{
-                  justifyContent: 'flex-start',
+                  justifyContent: "flex-start",
                   gap: 20,
                   paddingRight: 5,
-                  marginBottom: 10
+                  marginBottom: 10,
                 }}
-
                 className="mt-2 pb-32"
                 scrollEnabled={false}
               />
             </>
           </View>
         )}
-
-        
       </ScrollView>
     </View>
   );
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -26,9 +26,6 @@ const Search = () => {
         const timeoutId = setTimeout(async () => {
             if(searchQuery.trim()) {
                 await loadMovies();
-                if(movies?.length > 0 && movies?.[0]) {
-                    await updateSearchCount(searchQuery, movies[0]);
-                }
             } else {
                 reset()
             }
@@ -36,6 +33,12 @@ const Search = () => {
 
         return () => clearTimeout(timeoutId)
     }, [searchQuery])
+
+    useEffect(() => {
+        if(movies?.length > 0 && movies?.[0]) {
+            updateSearchCount(searchQuery, movies[0]);
+        }
+    }, [movies]);
     
     return (
         <View className="flex-1 bg-primary">
